ce1090360f0526b60b22f084c60b3e9e3a789179
abhilekh singh <abhilekhsingh@abhilekhs-MacBook-Pro.local>
2015-07-19 23:04:37 +0530

    =Added comments please check

--- Code/Stack/stack.cpp
+++ Code/Stack/stack.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 using namespace std;
 
+
+//Comment 1: class name should be lowercase for e.g vector that you are using
 class Stack{
 
 private:
@@ -13,10 +15,13 @@ public:
 	int top(); //returns element at top of stack
 	void pop(); //removes element at top of stack
 
+   //Comment 2: Refer this link for all operations
+   https://en.wikibooks.org/wiki/Data_Structures/Stacks_and_Queues
 };
+//Comment 3: Proper spacing
 bool Stack::empty()
 {
-    
+    //Comment 4: Refer Comment 3
     return list.empty();
 }
 
@@ -28,12 +33,15 @@ void Stack::push(int element)
 
 int Stack::top() 
 {
+   //Comment 5: This looks like a bug wat exactly you are trying to do here
 	if(list.back()>-1)
 	return list.back();
 
 	else
 		return -1;
 }
+
+//Comment 5: Refer Comment 3
 void Stack::pop() 
 {
 	list.pop_back();
@@ -46,11 +54,14 @@ int main()
    s.push(5);
    s.push(6);
    s.push(8);
-   t1=s.top();
+   t1=s.top();//poper naming of variable
    cout<<"top of element="<<t1;
    
    s.pop();
    s.pop();
    s.pop();
+
+   //Not a good test for checking you functionsa and not testes empty function here.
+
    return 0;
 }
