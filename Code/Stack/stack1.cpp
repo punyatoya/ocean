bebdb1cc2c0355427d1dbca26caf43ea3017e730
punyatoya <punyatoya213@gmail.com>
2015-07-25 00:26:36 +0530

    Added todo tasks and some improvement in stack

--- Code/Stack/stack1.cpp
+++ Code/Stack/stack1.cpp
@@ -1,90 +1,76 @@
 
 #include <iostream>
 #include <vector>
-//Always use space
+//Always use one space
 using namespace std;
+/*
+2 spaces
+*/
+//class name should be proper
+//Always in C++
+//word1_word2
+class stack {
+  private:
+    //Use Array
+  	vector<int> elements;
 
-
-
-class stackkk{
-
-private:
-	vector<int> stack_list;
-
-public:
-	bool is_empty(); //checks if stack is empty 
-	int get_stack_size();//gets the size of the stack
-	void push(int element); //adds element to stack
-	int top(); //returns element at top of stack
-	void pop(); //removes element at top of stack
-
+  public:
+  	bool empty(); //checks if stack is empty 
+  	int size();//gets the size of the stack
+  	void push(int element); //adds element to stack
+  	int top(); //returns element at top of stack
+  	void pop(); //removes element at top of stack
 };
-bool stackkk::is_empty()
-{
-	return stack_list.empty();
+
+bool stack::empty() {
+	return elements.empty();
 }
 
-void stackkk::push(int element) 
-{
-	stack_list.push_back(element);
+void stack::push(int element) {
+	elements.push_back(element);
 }
 
-int stackkk::top() 
-{
-	if(!(is_empty()))
-		return stack_list.back();
-	else
-	{
-		cout<<"\nSTACK IS EMPTY";
-                return (-1);
+int stack::top() {
+	if(!empty()) {
+		return elements.back();
+  } else {
+		return -1;
 	}
 }
-void stackkk::pop() 
-{
-	if(!(is_empty()))
-		stack_list.pop_back();
-	else
-        {
-		cout<<"\nSTACK IS EMPTY";
-        }
+
+void stack::pop() {
+	if(!empty()) {
+		elements.pop_back();
+  } 
 }
-int stackkk::get_stack_size()
-{
-	if(!(is_empty()))
-		return stack_list.size();
-	else
-	{
-		cout<<"\nSTACK IS EMPTY";
-                return (-1);
-	}
+
+int stack::size() {
+	return elements.size();
 }
+
+void print_stack_size(stack stack_obj) {
+  if(!stack_obj.empty()) { 
+       cout <<"\n size of the stack="<<stack_obj.size();
+   }
+}
+
 int main()
 {
-   stackkk stack1;
-   int top1;
-   stack1.push(5);
-   stack1.push(6);
-   stack1.push(8);
-   top1=stack1.top();
-   if(top1!=(-1))
-   { 
-       cout<<"\ntop of element="<<top1;
-       cout <<"\n size of the stack="<<stack1.get_stack_size();
-   }
-   stack1.pop();
-   stack1.pop();
-   top1=stack1.top();
-   if(top1!=(-1))
-   { 
-       cout<<"\ntop of element="<<top1;
-       cout <<"\n size of the stack="<<stack1.get_stack_size();
-   }
-   stack1.pop();
-   top1=stack1.top();
-   if(top1!=(-1))
-   { 
-       cout<<"\ntop of element="<<top1;
-       cout <<"\n size of the stack"<<stack1.get_stack_size();
-   }
+   stack stack_obj;
+
+   stack_obj.push(5);
+   stack_obj.push(6);
+   stack_obj.push(-1);
+   print_stack_size(stack_obj);
+
+   stack_obj.pop();
+   stack_obj.pop();
+   print_stack_size(stack_obj);
+
+   stack_obj.pop();
+   print_stack_size(stack_obj);
+
    return 0;
 }
+
+
